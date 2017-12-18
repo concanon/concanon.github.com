ca3c25b2f91195b739522be6f75dc949782de18c
spirit2change@gmail.com <spirit2change@gmail.com>
2017-12-17 19:48:06 -0800

    adding functor demo

---
+++ portfolio/js/lib_S2C_FunctorDemo.js
@@ -0,0 +1,62 @@
+
+function getLib_S2C_CodeSamplesPage()
+{
+	if(window.S2C_CodeSamplesPage){return S2C_CodeSamplesPage;}
+
+	var utils = getLib_S2C_Utils();
+	var makeFunctor = utils.makeFunctor;
+	
+	function basicFunctorDemo()
+	{
+		var adder = makeFunctor({a: 0, b: 0}, function(){return this.a + this.b;});
+		
+		alert('The result of adder() is: ' + adder());
+		
+		adder.a = 2;
+		adder.b = 3;
+		
+		alert('The new result of adder() is: ' + adder());
+	}
+	
+	function initButtonArea1()
+	{
+		var buttonAreaElt = document.getElementsByClassName('functorsArea')[0].getElementsByClassName('buttonArea')[0];
+		var btnElts = buttonAreaElt.getElementsByTagName('button');
+		
+		for(var i=0; i < 3; ++i)
+		{
+			btnElts[i].addEventListener('click', function(){alert('Performing action #' + i);});
+		}
+	}
+	
+	function initButtonArea2()
+	{
+		var buttonAreaElt = document.getElementsByClassName('functorsArea')[0].getElementsByClassName('buttonArea')[1];
+		var btnElts = buttonAreaElt.getElementsByTagName('button');
+		
+		for(var i=0; i < 3; ++i)
+		{
+			btnElts[i].addEventListener('click', 
+				makeFunctor(
+					{i: i},
+					function(){alert('Performing action #' + this.i);}
+				)
+			);
+		}
+	}
+	
+	
+	function init()
+	{
+		initButtonArea1();
+		initButtonArea2();
+	}
+
+	
+	
+	return window.S2C_CodeSamplesPage = {
+		init: init,
+		basicFunctorDemo: basicFunctorDemo
+	};
+}
+
